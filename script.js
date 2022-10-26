20ec21b9002a20e33e556c1c723ad3ed29c4eb8e
Abegail Pates <abegail.pates@gmail.com>
2022-10-26 17:53:31 +0800

    Add game board method that will check for a winner

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gameBoard = (() => {
     const board = [
-        ['','','',],
-        ['','','',],
-        ['','','',]
+        ['o','x','o',],
+        ['x','o','o',],
+        ['o','o','x',]
     ]
     const addMarker = (marker, xIndex, yIndex) => {
         if(board[xIndex][yIndex] !== '') {
@@ -12,9 +12,34 @@ const gameBoard = (() => {
             return true;
         }
     }
+    const checkForWinner = () => {
+        let hasWinner = false;
+        //checking rows
+        for (let x = 0; x < 3; x++) {
+            if (board[x][0] !== '' && board[x][0] === board[x][1] && board[x][1] === board[x][2]) {
+                hasWinner = true;
+                break;
+            }
+        }
+        //checking columns
+        for (let y = 0; y < 3; y++) {
+            if (board[0][y] !== '' && board[0][y] === board[1][y] && board[1][y] === board[2][y]) {
+                hasWinner = true;
+                break;
+            }
+        }
+        //checking diagonals
+        if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+            hasWinner = true;
+        } else if (board[2][0] !== '' && board[2][0] === board[1][1] && board[1][1] === board[0][2]) {
+            hasWinner = true;
+        }
+        return hasWinner;
+    }
     return {
         board,
         addMarker,
+        checkForWinner
     }
 })();
 
